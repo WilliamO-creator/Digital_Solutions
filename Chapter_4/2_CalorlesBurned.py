ad122e30831b4702c866ff58ca7e5546a80421e8
WilliamO-creator <williamolufson@icloud.com>
2020-03-30 15:15:38 +1000

    class work

---
+++ Chapter_4/2_CalorlesBurned.py
@@ -0,0 +1,7 @@
+time = int(input("time on treadmil in minutes"))
+cal = 0
+i = 0
+while i < time:
+    cal = cal + 4.2
+    i = i + 1
+print("calorles burned", cal, "J")
---
+++ Chapter_4/3.LapTimes.py
@@ -0,0 +1,7 @@
+laps = int(input("how many laps have you done"))
+i = 0
+laptime = 0
+while i < laps:
+    time = int(input("enter your time for your lap"))
+    i = i + 1
+    laptime
