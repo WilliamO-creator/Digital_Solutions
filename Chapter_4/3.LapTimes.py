523a700a27da8743cc1aa86978f0e0f022ff7689
WilliamO-creator <williamolufson@icloud.com>
2020-04-02 12:38:22 +1000

    arvage rain fall

--- Chapter_4/3.LapTimes.py
+++ Chapter_4/3.LapTimes.py
@@ -1,7 +1,23 @@
+
 laps = int(input("how many laps have you done"))
 i = 0
+times = set()
 laptime = 0
+def MAX(times):
+    return (max(times))
+def Min(times):
+    return (min(times))
+def averageOfList(times):
+    sumOfNumbers = 0
+    for t in times:
+        sumOfNumbers = sumOfNumbers + t
+
+    avg = sumOfNumbers / len(times)
+    return avg
 while i < laps:
     time = int(input("enter your time for your lap"))
     i = i + 1
-    laptime = laptime
+    times.add(time)
+print("The fastest time is ", Min(times))
+print("The slowest time is ", MAX(times))
+print("Avrage lap time is ", averageOfList(times))
---
+++ Chapter_4/7.PenniesForPay.py
@@ -0,0 +1,10 @@
+i = 0
+t = int(input("number of days"))
+total = 0
+while i < t:
+    i = i + 1
+    pen = int(input("enter money in Pennes"))
+    total = total + pen
+    print("day", i, "money earned $", pen*0.01)
+    if i == 1 and
+print("Total pay earned over the period of", i, "days is $", total*0.01,)
